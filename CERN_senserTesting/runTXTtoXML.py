66f328979d08867c9823ce0cb301df03c73399ec
huiling110 <huiling2016@outlook.com>
2022-10-18 15:44:22 +0200

    cern input iv conversion good

--- CERN_senserTesting/runTXTtoXML.py
+++ CERN_senserTesting/runTXTtoXML.py
@@ -7,10 +7,10 @@ def main():
     # data from analysis frame work in pclcd15: /home/data/hgsensor_iv/
     # /home/data/hgsensor_iv/Hamamatsu_Preseries_June2022
     # inputTxtDir = '/Users/hua/mountFolderForpcd/hgsensor_iv/Hamamatsu_Preseries_June2022/temperature_scaled/'
-    # inputTxtDir = '/eos/user/h/hgsensor/HGCAL_test_results/Results/Preseries_June2022/HPK_8in_444ch_300048/'
-    # inputFile = 'HPK_8in_444ch_300048_IV.txt'
-    inputTxtDir = '../'
-    inputFile = 'HPK_8in_198ch_2019_N4792_18_03242022_FullRetest_IV.txt'
+    inputTxtDir = '/eos/user/h/hgsensor/HGCAL_test_results/Results/Preseries_June2022/HPK_8in_444ch_300048/'
+    inputFile = 'HPK_8in_444ch_300048_IV.txt'
+    # inputTxtDir = '../'
+    # inputFile = 'HPK_8in_198ch_2019_N4792_18_03242022_FullRetest_IV.txt'
     inputTxt = inputTxtDir + inputFile
 
     TXT_TO_XML.make_xml_schema_HGC_CERN_SENSOR_IV(inputTxt)
